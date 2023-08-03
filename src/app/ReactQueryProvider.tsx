27a0153e9af705d58e4677a5e4d78c141d670be1
ViniciusCLoeblein <vinicius.carmo@grazziotin.com.br>
2023-08-03 17:47:13 -0300

    ajuste

---
+++ src/app/ReactQueryProvider.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import React, { ReactNode } from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
+export const ReactQueryProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ReactQueryProvider } from './ReactQueryProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,14 +9,16 @@ export const metadata: Metadata = {
   description: 'Nova intranet da Grazziotin para lojas e ADM',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ReactQueryProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ReactQueryProvider>
   )
 }
