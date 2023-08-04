418889cbb3daf85835d20f1e9273cd728a279645
flaviasoz <flavia.souza@grazziotin.com.br>
2023-08-04 14:54:22 -0300

    att

--- src/app/cookies/auth.ts
+++ src/app/cookies/auth.ts
@@ -2,11 +2,5 @@
 import { cookies } from 'next/headers'
 
 export default async function setUser(data: string) {
-  cookies().set('LOGIN_INFO', data, {
-    priority: 'high',
-    // expires: 1,
-    // sameSite: 'strict',
-    //   httpOnly: false,
-    //   secure: false,
-  })
+  cookies().set('LOGIN_INFO', data)
 }
